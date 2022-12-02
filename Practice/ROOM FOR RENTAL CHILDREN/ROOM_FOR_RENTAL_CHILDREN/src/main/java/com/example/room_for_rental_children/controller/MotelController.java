f85f6d3ec643e3d5c0ae05d450ab46fb5d0122e2
trinhminhduc9x <duc1997th@gmail.com>
2022-12-02 16:32:36 +0700

    add

--- Practice/ROOM FOR RENTAL CHILDREN/ROOM_FOR_RENTAL_CHILDREN/src/main/java/com/example/room_for_rental_children/controller/MotelController.java
+++ Practice/ROOM FOR RENTAL CHILDREN/ROOM_FOR_RENTAL_CHILDREN/src/main/java/com/example/room_for_rental_children/controller/MotelController.java
@@ -3,13 +3,18 @@ package com.example.room_for_rental_children.controller;
 import com.example.room_for_rental_children.model.MotelRoom;
 import com.example.room_for_rental_children.service.IMotelRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/motelRoom")
@@ -30,12 +35,34 @@ public class MotelController {
 //    }
 
 
-//    hiển thị list với câu tìm kiếm
+//    hiển thị list với phan trang
 
     @RequestMapping("/list")
     private String showList(Model model,@PageableDefault(page = 0,size = 1) Pageable pageable){
         model.addAttribute("motelRoom",iMotelRoomService.findPageAll(pageable));
         return "MotelRoom/list";
     }
+    //    hiển thị list với câu tìm kiếm
+    @GetMapping("/list")
+    public String goPage(Model model,
+                         @PageableDefault(6) Pageable pageable,
+                         @RequestParam Optional<String> name,
+                         @RequestParam(required = false, defaultValue = "") String email,
+                         @RequestParam(required = false, defaultValue = "") String CustomerTypeID) {
+        for (Sort.Order order : pageable.getSort()) {
+            model.addAttribute("sortValue", order.getProperty());
+        }
+        String keyName = name.orElse("");
 
+        Page<MotelRoom> motelRooms = iMotelRoomService.findPageNameEmailType(pageable, keyName, email, CustomerTypeID);
+
+        List<CustomerType> customerTypeList = customerTypeService.findListAll();
+
+
+        model.addAttribute("customerTypeList", customerTypeList);
+        model.addAttribute("customerPage", customerPage);
+        model.addAttribute("CustomerTypeID", CustomerTypeID);
+        model.addAttribute("name", keyName);
+        model.addAttribute("email", email);
+    }
 }
--- Practice/ROOM FOR RENTAL CHILDREN/ROOM_FOR_RENTAL_CHILDREN/src/main/java/com/example/room_for_rental_children/repository/MotelRoomRepository.java
+++ Practice/ROOM FOR RENTAL CHILDREN/ROOM_FOR_RENTAL_CHILDREN/src/main/java/com/example/room_for_rental_children/repository/MotelRoomRepository.java
@@ -1,9 +1,17 @@
 package com.example.room_for_rental_children.repository;
 
 import com.example.room_for_rental_children.model.MotelRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface MotelRoomRepository extends JpaRepository<MotelRoom,Integer> {
+    @Query(value = " select *  from motel_room where motel_room.date_start like '2022-01-02' and motel_room.date_end like '2022-02-17'; ", nativeQuery = true)
+    Page<MotelRoom> findPageAll(Pageable pageable, @Param("name") String name, @Param("email") String email, @Param("CustomerTypeID") String CustomerTypeID);
+
+
 }
