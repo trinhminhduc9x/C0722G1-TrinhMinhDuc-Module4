381a32d960ad2324e92ac52b9d440e6426c32ae5
trinhminhduc9x <duc1997th@gmail.com>
2022-12-07 17:16:34 +0700

    Add

--- Practice/ROOM FOR RENTAL CHILDREN/ROOM FOR RENTAL CHILDREN/ROOM_FOR_RENTAL_CHILDREN/src/main/java/com/example/room_for_rental_children/service/IChildService.java
+++
@@ -1,9 +0,0 @@
-package com.example.room_for_rental_children.service;
-
-import com.example.room_for_rental_children.model.ChildHouseHold;
-
-import java.util.List;
-
-public interface IChildService {
-    List<ChildHouseHold>FindListAll();
-}
